45ef96f856c64aaf6a60b0c3afcb7de4aef312e2
Mario Šparica <mario.sparica@flipkod.com>
2023-05-24 18:37:11 +0200

    empty commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,6 @@ const router = createBrowserRouter([
 	}
 ]);
 
-function App() {
-	return <RouterProvider router={router} />;
-}
+function App() { return <RouterProvider router={router} />; }
 
 export default App;
